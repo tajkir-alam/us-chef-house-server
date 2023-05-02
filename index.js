459ecdb5cb2099121f8a8cba0196fcbbefd69e16
tajkir-alam <tajkir.alam@gmail.com>
2023-05-03 03:11:20 +0600

    chef about json file created

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 app.use(cors());
 
 const chefInfo = require('./data/chefinfo.json')
-
+const chefAbout = require('./data/aboutchef.json')
 
 app.get('/', (req, res) => {
   res.send('Type chefinfo after /')
@@ -15,6 +15,12 @@ app.get('/', (req, res) => {
 app.get('/chefinfo', (req, res) => {
   res.send(chefInfo)
 })
+
+app.get('/chef-about', (req, res) => {
+  res.send(chefAbout)
+})
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
